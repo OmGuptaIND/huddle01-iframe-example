6dd9315b3d7670090e66f1b1c56a1847dff6731c
OmGuptaIND <omgupta0720@gmail.com>
2023-02-28 16:01:10 +0530

    add: change avatarUrl

--- src/Components/IFrameComponent.tsx
+++ src/Components/IFrameComponent.tsx
@@ -12,7 +12,7 @@ const IFrameComponent = () => {
     huddleIframeApp.on(HuddleAppEvent.PEER_JOIN, () => {
       // eslint-disable-next-line @typescript-eslint/no-unsafe-call
       huddleIframeApp.methods.changeAvatarUrl(
-        "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80"
+        "https://i.imgur.com/0X0X0X0.png"
       );
     });
   }, []);
